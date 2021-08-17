1395f497309f624b90f76ba74f052388d7e0bb85
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-08-17 09:36:42 -0400

    add providers

---
+++ lib/providers/busy_provider.dart
@@ -0,0 +1,32 @@
+// 🐦 Flutter imports:
+import 'package:flutter/foundation.dart';
+
+// 🌎 Project imports:
+import 'package:recipe_app/providers/provider_state.dart';
+
+abstract class BusyProvider extends ChangeNotifier {
+  BusyProvider() : _status = ProviderStatus.done;
+
+  ProviderStatus _status;
+
+  bool get isLoading => _status == ProviderStatus.loading;
+
+  /// Toggles the provider from [ProviderStatus.done] to [ProviderStatus.loading] and vice-versa
+  void toggleBusy() {
+    _status = _status == ProviderStatus.done
+        ? ProviderStatus.loading
+        : ProviderStatus.done;
+    notifyListeners();
+  }
+
+  /// Busy is set to [ProviderStatus.done]
+  void reset() => _status = ProviderStatus.done;
+
+  void startBusy() => _status = ProviderStatus.loading;
+
+  @override
+  void dispose() {
+    reset();
+    super.dispose();
+  }
+}
---
+++ lib/providers/providers.dart
@@ -0,0 +1,40 @@
+/* List with initialization of all providers */
+
+// 📦 Package imports:
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+// 🌎 Project imports:
+
+/// all providers being used in the application
+final providers = <SingleChildWidget>[
+  ..._authProviders,
+  ..._functionalityProviders,
+];
+
+/// All providers being used through user management, sign in and sign out
+final _authProviders = <SingleChildWidget>[
+  // ChangeNotifierProvider<SignInProvider>(
+  //   create: (_) => SignInProvider(),
+  // ),
+  // ChangeNotifierProvider<ResetPasswordProvider>(
+  //   create: (_) => ResetPasswordProvider(),
+  // ),
+  // ChangeNotifierProvider<AuthProvider>(
+  //   create: (_) => AuthProvider(),
+  // ),
+  // ChangeNotifierProvider<ChangePasswordProvider>(
+  //   create: (_) => ChangePasswordProvider(),
+  // )
+];
+
+/// Providers that may or may not require an token, but provides functionality on the scope
+/// of the app domains
+final _functionalityProviders = <SingleChildWidget>[
+  // ChangeNotifierProvider<PushNotificationProvider>(
+  //   create: (_) => PushNotificationProvider(),
+  // ),
+  // ChangeNotifierProvider<HomeEventProvider>(
+  //   create: (_) => HomeEventProvider(),
+  // ),
+];
